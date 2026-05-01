f6dce2d1df190a4fb2e7f32960e556127cc106b7
Saurav Singh <sauravsingh98866@gmail.com>
2026-05-02 04:51:21 +0530

    fix: all missing .js extensions in imports

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
-import product from "../models/product";
+import Product from "../models/product.js";  
 
 //Add Product: /api/product/add
 export const addProduct = async (req, res) => {
